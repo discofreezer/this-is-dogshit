dc9fb82bc0bd0b62b11455a5abbdebaeaaabd9fd
Discofreezer <hanul2255@gmail.com>
2023-12-19 17:47:42 -0500

    Changed code to make upgrades work (I think?), corrected numerical  mistakes in mana files. Added status effects for five elements, will be used in "combination effects" (e.g. fire onto wood causes an explosion, removes wood effect) Added first elementalist spell, mostly the same. Did not test final project, pushing so I can access at another house.

---
+++ src/main/java/net/reimaden/touhouorigins/effect/MagiwaterEffect.java
@@ -0,0 +1,21 @@
+package net.reimaden.touhouorigins.effect;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class MagiwaterEffect extends StatusEffect {
+    public MagiwaterEffect(StatusEffectCategory statusEffectCategory, int color) {
+        super(statusEffectCategory, color);
+    }
+    @Override
+    public void applyUpdateEffect(LivingEntity pLivingEntity, int pAmplifier) {
+
+        super.applyUpdateEffect(pLivingEntity, pAmplifier);
+    }
+
+    @Override
+    public boolean canApplyUpdateEffect(int pDuration, int pAmplifier) {
+        return true;
+    }
+}
--- src/main/java/net/reimaden/touhouorigins/effect/ModEffects.java
+++ src/main/java/net/reimaden/touhouorigins/effect/ModEffects.java
@@ -9,13 +9,45 @@ import net.reimaden.touhouorigins.TouhouOrigins;
 
 public class ModEffects {
     public static StatusEffect MANAPOWER;
+    public static StatusEffect MAGIFIRE;
+    public static StatusEffect MAGIWATER;
+    public static StatusEffect MAGIWOOD;
+    public static StatusEffect MAGIMETAL;
+    public static StatusEffect MAGIEARTH;
 
     public static StatusEffect registerStatusEffect(String name) {
         return Registry.register(Registries.STATUS_EFFECT, new Identifier(TouhouOrigins.MOD_ID, name),
                 new ManaPowerEffect(StatusEffectCategory.BENEFICIAL, 3052277));
     }
+    public static StatusEffect registerMagifireEffect(String name) {
+        return Registry.register(Registries.STATUS_EFFECT, new Identifier(TouhouOrigins.MOD_ID, name),
+                new MagifireEffect(StatusEffectCategory.HARMFUL, 16718592));
+    }
+    public static StatusEffect registerMagiwaterEffect(String name) {
+        return Registry.register(Registries.STATUS_EFFECT, new Identifier(TouhouOrigins.MOD_ID, name),
+                new MagiwaterEffect(StatusEffectCategory.HARMFUL, 16718592));
+    }    public static StatusEffect registerMagiwoodEffect(String name) {
+        return Registry.register(Registries.STATUS_EFFECT, new Identifier(TouhouOrigins.MOD_ID, name),
+                new MagiwoodEffect(StatusEffectCategory.HARMFUL, 4298752));
+    }    public static StatusEffect registerMagimetalEffect(String name) {
+        return Registry.register(Registries.STATUS_EFFECT, new Identifier(TouhouOrigins.MOD_ID, name),
+                new MagimetalEffect(StatusEffectCategory.HARMFUL, 13421772));
+    }    public static StatusEffect registerMagiearthEffect(String name) {
+        return Registry.register(Registries.STATUS_EFFECT, new Identifier(TouhouOrigins.MOD_ID, name),
+                new MagiearthEffect(StatusEffectCategory.HARMFUL, 7757073));
+    }
 
     public static void registerEffects() {
         MANAPOWER = registerStatusEffect("manapower");
+        MAGIFIRE = registerMagifireEffect("火");
+        MAGIWATER = registerMagiwaterEffect("水");
+        MAGIWOOD = registerMagiwoodEffect("木");
+        MAGIMETAL = registerMagimetalEffect("金");
+        MAGIEARTH = registerMagiearthEffect("土");
+
+
     }
+
+
+
 }
